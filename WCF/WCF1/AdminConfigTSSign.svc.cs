07173bf5061627e9ed269bc4bbbe1a96d0490935
kmzs <karsten.meyerzuselhausen@rub.de>
2016-10-03 17:35:53 +0200

    WCF web services added

---
+++ WCF/WCF1/AdminConfigTSSign.svc.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Copyright 2016 Karsten Meyer zu Selhausen
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF
+{
+    public class AdminConfigTSSign : IAdminConfigTSSign
+    {
+        public string getAdminToken()
+        {
+            string date = DateTime.Now.ToString("dd'/'MM'/'yyyy HH:mm:ss");
+            Console.WriteLine(date + ": getAdminToken() called (TSSign)");
+            return sha256(date);
+        }
+
+        public string getServerTime()
+        {
+            string date = DateTime.Now.ToString("dd'/'MM'/'yyyy HH:mm:ss");
+            Console.WriteLine(date + ": getServerTime() called (TSSign)");
+            return date;
+        }
+
+        //Original source: stackoverflow.com/questions/12416249/hashing-a-string-with-sha256
+        private static string sha256(string input)
+        {
+            System.Security.Cryptography.SHA256Managed hashfunction = new System.Security.Cryptography.SHA256Managed();
+            byte[] hash_bytes = hashfunction.ComputeHash(Encoding.UTF8.GetBytes(input), 0, Encoding.UTF8.GetByteCount(input));
+            System.Text.StringBuilder hash_string = new StringBuilder();
+            foreach (byte theByte in hash_bytes)
+            {
+                hash_string.Append(theByte.ToString("x2"));
+            }
+            return hash_string.ToString();
+        }
+    }
+}
---
+++ WCF/WCF1/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright 2016 Karsten Meyer zu Selhausen
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace WCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = null;
+            ServiceHost tSSignHost = null;
+            ServiceHost encTSSignHost = null;
+            try
+            {
+                serviceHost = new ServiceHost(typeof(AdminConfig));
+                serviceHost.Open();
+                Console.WriteLine("No Security web service is online. WSDL: http://localhost:8081/?wsdl");
+                tSSignHost = new ServiceHost(typeof(AdminConfigTSSign));
+                tSSignHost.Open();
+                Console.WriteLine("TS+Sign web service is online. WSDL: http://localhost:8082/?wsdl");
+                encTSSignHost = new ServiceHost(typeof(AdminConfigEncTSSign));
+                encTSSignHost.Open();
+                Console.WriteLine("Enc+TS+Sign web service is online. WSDL: http://localhost:8083/?wsdl");
+                Console.WriteLine("Press ENTER to shutdown the services.");
+                Console.ReadLine();
+                serviceHost.Close();
+                tSSignHost.Close();
+                encTSSignHost.Close();
+            }
+            catch (Exception ex)
+            {
+                serviceHost = null;
+                tSSignHost = null;
+                encTSSignHost = null;
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press ENTER to shutdown the services.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
